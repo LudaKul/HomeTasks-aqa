0c057c152ceb7e73fbd042d151a6949b82e94b81
LudaKul <kulybaluda.92@gmail.com>
2025-10-31 12:04:25 +0300

    Add Fibonacci solution

---
+++ src/HomeTaskFibonacci.java
@@ -0,0 +1,14 @@
+public class HomeTaskFibonacci {
+    public static void main(String[] args) {
+        System.out.println("Fibonacci");
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i <11; i++){
+            System.out.println(a);
+            int c = a + b;
+            System.out.println("Iteration: " + c);
+            a = b;
+            b = c;
+        }
+    }
+}
